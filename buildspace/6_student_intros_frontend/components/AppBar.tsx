8c94e92783db43cf75936b93c85e87d344d63e49
Tobias Leinss <caruso33@web.de>
2022-11-16 14:27:49 +0100

    Add solana movie clients

---
+++ buildspace/6_student_intros_frontend/components/AppBar.tsx
@@ -0,0 +1,20 @@
+import dynamic from "next/dynamic"
+import Image from "next/image"
+import { FC } from "react"
+import styles from "../styles/Home.module.css"
+
+export const AppBar: FC = () => {
+  const WalletMultiButton = dynamic(
+    async () =>
+      (await import("@solana/wallet-adapter-react-ui")).WalletMultiButton,
+    { ssr: false }
+  )
+
+  return (
+    <div className={styles.AppHeader}>
+      <Image src="/solanaLogo.png" height={30} width={200} />
+      <span>Student Intros</span>
+      <WalletMultiButton />
+    </div>
+  )
+}
